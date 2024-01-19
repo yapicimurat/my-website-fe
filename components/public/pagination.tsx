1356186f4a6d1d267b8a80f8f9d2f0b4d4ef7572
yapicimurat <muratyapiicii@gmail.com>
2024-01-19 23:23:44 +0300

    fix pagination

--- components/public/pagination.tsx
+++ components/public/pagination.tsx
@@ -2,73 +2,118 @@ import {Article} from "../../service/model/article";
 import Pageable from "../../service/model/pageable";
 import {useEffect, useState} from "react";
 import clsx from "clsx";
+import {clamp} from "../../util/util";
 
 type PaginationProps = {
     pageable: Pageable<Article>
 }
 
-const MAX_RIGHT_NODE_NUMBER = 8;
-const AMOUNT_OF_SINGLE_NODE_BUTTONS = 8;
-const THREE_POINTS = <li className="page-item"><a className="page-link" href="#">...</a></li>;
-
-
 export default function Pagination(props: PaginationProps) {
     const [current, setCurrent] = useState(1);
 
-    const drawFullNodeButtons = (endNumber: number) => {
-        return Array(endNumber).fill(0).map((x, index) => {
-            return (
-                <li className={clsx({"page-item": true, "active": current === index + 1})}><a className="page-link" href="#">{index + 1}</a></li>
-            )
-        });
-    }
+    const calcAmountOfPage = Math.ceil(props.pageable.totalElementsPerPage * props.pageable.totalPages / props.pageable.totalElementsPerPage);
+    const amountOfPage = (calcAmountOfPage >= 1) ? calcAmountOfPage : 1;
 
-    const drawPageButton = () => {
-        //
-        if(props.pageable.totalPages <= 10) {
-            return drawFullNodeButtons(props.pageable.totalPages);
-        }else {
-            //decide pagination bar will be single left, single right or double node
-
-            //left node
-            //SINGLE NODE
-            //if(props.pageable.totalPages -  (AMOUNT_OF_SINGLE_NODE_BUTTONS + 1))
-
-
-            //right node
-
-
-            //double node
-        }
-
-
-
-
+    const goPage = (pageNumber: number) => {
+        setCurrent(pageNumber);
     };
 
-    useEffect(() => {
-        const size = props.pageable.elements.length;
+    const paginationItem = (pageNumber: number) => {
+        return (
+            <li key={pageNumber} style={{width: "40px"}} className={clsx({"text-center": true, "page-item": true, "active": (pageNumber === current)})}>
+                <a role="button" className="page-link user-select-none" onClick={() => goPage(pageNumber)}>{pageNumber}</a>
+            </li>
+        );
+    }
 
-    }, [props.pageable]);
+    const next = () => {
+        const newPage = clamp(current + 1, 1, amountOfPage);
+        setCurrent(newPage);
+    };
+    const previous = () => {
+        const newPage = clamp(current - 1, 1, amountOfPage)
+        setCurrent(newPage);
+    };
 
-    return (
-        <nav aria-label="Page navigation example">
-            <ul className="pagination justify-content-end">
-                <li className={clsx({'page-item': true, 'disabled': !props.pageable.hasPrevious})}>
-                    <a className="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
+    const drawPageButton = () => {
+        const generatedPageItems = [];
+        if(amountOfPage <= 10){
+            debugger;
+            for(let i = 1; i <= amountOfPage; i++){
+                generatedPageItems.push(paginationItem(i));
+            }
+        }else{
+            debugger;
+            const isDoubleNode = (current - 3 > 2 && current + 4 < amountOfPage - 1);
+            const isLeftNode = (current - 3 > 2);
+            const isRightNode = (current + 4 < amountOfPage - 1);
+            const node = (
+                <li className="page-item">
+                    <p className="page-link text-dark">...</p>
                 </li>
-                {
-                    drawPageButton()
+            );
+
+            if(isDoubleNode){
+                generatedPageItems.push(
+                    <>
+                        {paginationItem(1)}
+                        {node}
+                    </>
+                );
+
+                for(let i = current - 3; i <= (current - 3) + 7; i++){
+                    generatedPageItems.push(paginationItem(i));
                 }
 
-                <li className={clsx({'page-item': true, 'disabled': !props.pageable.hasNext})}>
-                    <a className="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+                generatedPageItems.push(
+                    <>
+                        {node}
+                        {paginationItem(amountOfPage)}
+                    </>
+                );
+            }
+            else if(isLeftNode){
+                generatedPageItems.push(
+                    <>
+                        {paginationItem(1)}
+                        {node}
+                    </>
+                );
+
+                for(let i = amountOfPage - 8; i <= amountOfPage; i++){
+                    generatedPageItems.push(paginationItem(i))
+                }
+
+            }
+            else if(isRightNode){
+                for(let i = 1; i <= 1 + 8; i++){
+                    generatedPageItems.push(paginationItem(i))
+                }
+
+                generatedPageItems.push(
+                    <>
+                        {node}
+                        {paginationItem(amountOfPage)}
+                    </>
+                );
+            }
+        }
+        return generatedPageItems;
+    };
+
+    return (
+        <div className="row">
+            <div className="col-12 d-flex flex-row justify-content-end">
+                <nav className="mb-1" aria-label="Page navigation example">
+                    <ul className="pagination pagination-sm">
+                        <li className="page-item"><a role="button" className="page-link user-select-none"
+                                                     onClick={previous}>&laquo;</a></li>
+                        {drawPageButton()}
+                        <li className="page-item"><a role="button" className="page-link user-select-none"
+                                                     onClick={next}>&raquo;</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     );
 }
--- util/util.ts
+++ util/util.ts
@@ -1,3 +1,7 @@
 export const clampStr = (str: string, max: number): string => {
     return (str.length >= max) ? str.substring(0, max) + "..." : str;
 }
+
+export const clamp = (val: number, min: number, max: number): number => {
+    return Math.min(Math.max(val, min), max);
+}
