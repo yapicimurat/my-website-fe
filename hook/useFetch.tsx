e1b468819547b1e974cdc5384e925507b4371d2a
PICASSO <muratyapiicii@gmail.com>
2024-02-18 23:35:46 +0300

    Add dynamic parameters for useFetch parameter function

--- hook/useFetch.tsx
+++ hook/useFetch.tsx
@@ -1,13 +1,13 @@
 import {useEffect, useState} from "react";
 
-export default function useFetch<T>(fetchMethod: () => Promise<T>) {
+export default function useFetch<T>(fetchMethod: ({}: any) => Promise<T>, params: {}) {
     const [data, setData] = useState<T>();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             setLoading(true);
-            setData(await fetchMethod());
+            setData(await fetchMethod(params));
 
             setTimeout(() => {
                 setLoading(false);
